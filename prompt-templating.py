3b2cbd057830f65e3941739855d79a63adcacbf7
Juan Montilla <juan.montilla@southworks.com>
2023-11-11 07:37:47 -0300

    prompt-templating

---
+++ prompt-templating.py
@@ -0,0 +1,30 @@
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import AzureChatOpenAI
+from dotenv import load_dotenv
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+load_dotenv()
+
+llm = AzureChatOpenAI(
+    deployment_name="gpt-4",
+    model_name="gpt-4",
+)
+
+prompt=PromptTemplate(
+    template="You are a helpful assistant that translates {input_language} to {output_language}.",
+    input_variables=["input_language", "output_language"],
+)
+system_message_prompt = SystemMessagePromptTemplate(prompt=prompt)
+human_template="{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+# get a chat completion from the formatted messages
+output = llm(chat_prompt.format_prompt(input_language="English", output_language="French", text="I love programming.").to_messages())
+print(output) # content="J'aime la programmation."
