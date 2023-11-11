71f0663e91f75c250831015e1e706512ee9afc49
Juan Montilla <juan.montilla@southworks.com>
2023-11-11 07:24:22 -0300

    in-memory-cache

---
+++ in-memory-cache.py
@@ -0,0 +1,24 @@
+from langchain.globals import set_llm_cache
+from langchain.cache import InMemoryCache
+from langchain.chat_models import AzureChatOpenAI
+from dotenv import load_dotenv
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage
+)
+
+load_dotenv()
+
+llm = AzureChatOpenAI(
+    deployment_name="gpt-4",
+    model_name="gpt-4",
+)
+set_llm_cache(InMemoryCache())
+
+# The first time, it is not yet in cache, so it should take longer
+output = llm.predict("Tell me a joke")
+print(output)
+
+# The second time it is, so it goes faster
+output = llm.predict("Tell me a joke")
+print(output)
